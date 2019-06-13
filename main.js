3637ec527e959140b1643ecdb91a1dfe7a445d6f
Cristian Felipe <cristianpipe0494@gmail.com>
2019-06-13 14:17:23 -0600

    Have added functionality to generate confeti and move them. Also added constructor to generate them.

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ const _imageArray = new Array(
 
     // Character
     // /////////
+    'eye.png',
     'body.png',
 
-    // Character
+    // Car
     // /////////
     'openDoor.png',
     'nutmovil.png',
@@ -21,12 +22,12 @@ const _imageArray = new Array(
     'catchPhrase-final.png',
     'catchPhrase-intro.png',
     'catchPhrase-preFinal.png',
+    'catchPhrase-action.png',
 );
 
 // Rect Values (0px: Top Value, 0px: Right Value, 0px: Bottom Value, 0px: Left Value).
 // Arrays with starting values and ending values for transitions.
 const x = ['rect(0px, 0px, 0px, 0px)', 'rect(0px, 0px, 0px, 0px)'];
-
 
 this.addEventListener('DOMContentLoaded', preloadImages);
 
@@ -57,103 +58,157 @@ function initAnimations(){
     _tlShowing
     .set('.banner',{display: 'block'})
 
-    // Title and show animation in and out.
-    // /////////
-    .from(['.main-title-position','.main-title-shade-position'], 1,{ top: ('-500')})
-    .addLabel('bezierMovement')
-    .to('.main-title-position', 2, {bezier:{curviness:2, values:[{x:('-5'), y:('-2')}, {x:('-5'), y:('10')}, {x:('0'), y:('-10')}, {x:('5'), y:('5')}]}, ease:Power1.easeInOut, yoyo: true, repeat: 1}, 'bezierMovement')
-    .to('.main-title-shade-position', 2, {bezier:{curviness:2, values:[{x:('5'), y:('2')}, {x:('5'), y:('-10')}, {x:('0'), y:('10')}, {x:('-5'), y:('-5')}]}, ease:Power1.easeInOut, yoyo: true, repeat: 1}, 'bezierMovement')
-    .to(['.main-title-position','.main-title-shade-position', '.juice-shadow-position', '.juice-details-position'], 2,{ top: ('-150'), opacity: ('0')})
-    // /////////
-
-    // Border and Deer animation in.
+    // Intro car.
     // //////////
-    .from(['.deer-body-position', '.deer-head-position'], 0.5, { top: ('300')})
-    .from(['.border-line-width', '.orange-banner-position'], 0.5, { top: ('300')})
+    .to('.first-phrase-position', 1, {opacity: ('0')}, '+=1.5')
+    .from('.nutmovile-position', 1, {left: ('300'), top: ('-16'), rotation: ('5')}, '-=1')
+    .to('.nutmovile-position', 1, {ease: Bounce.easeOut ,rotation: ('-2')})
+    // Intro car.
     // //////////
 
-    // Deer moving Head animation.
-    // ///////////
-    .to('.deer-head-position', 1, {top: ('40'), left: ('76'), rotation: ('-21deg')}).delay(0.5)
-    .to('.deer-head-position', 1, {top: ('38'), left: ('92'), rotation: ('1deg'),}).delay(0.5)
-    // ///////////
-
-    // Deer moving Head and Body animation.
-    // ///////////
-    .addLabel('movingWith')
-    .to('.deer-body-position', 1, {top: ('120'), left: ('95'), rotation:('-4deg')}, 'movingWith')
-    .to('.deer-head-position', 1, {top: ('40'), left: ('74'), rotation: ('-21deg'),}, 'movingWith')
-    // ///////////
-
-    // Falling product animation
-    // ///////////////
-    .addLabel('smashingTo', '+=0.2')
-    .addLabel('shadowFalling')
-    .from('.productbox-felt-position', 1,{
-        ease: Bounce.easeOut,
-        top: ('-1000'),
-        height: ('160'),
-        width: ('209'),
-        top: ('-195'),
-        bottom: ('0'),
-        left: ('15'),
-        rotation: ('4'),
-        zIndex: ('4')      
-    },'shadowFalling')
-    .from('.product-shadow-blur', 0.5, {ease: Bounce.easeOut, top: ('-200'), opacity: ('0')}, 'shadowFalling')
-    // ///////////////
-
-    // Smashing Head and Body animation.
+    // Catch Phrase.
     // /////////////
-    .set('.deer-head-position', {clip: headGone[0]}, 'smashingTo')
-    .to('.deer-head-position', 0.1, {clip: headGone[1], opacity: ('0')}, 'smashingTo')
-    .set('.deer-body-position', {clip: bodyGone[0]}, 'smashingTo')
-    .to('.deer-body-position', 0.1, {clip: bodyGone[1], opacity: ('0')}, 'smashingTo')
-    // /////////////
-
-    // Character jumping from product.
-    // /////////////////
-    .addLabel('showingDeer', '+=0.2')
-    .to('.charact-appear-position', 0.5, {clip: appearCharact[0]},'smashingTo')
-    .to('.charact-appear-position', 0.5, {clip: appearCharact[1], opacity: ('1')},'showingDeer')
-
-    .to('.wrapper-front-position', 0.5, {clip: frontWrap[0]},'smashingTo')
-    .to('.wrapper-front-position', 0.5, {clip: frontWrap[1], opacity: ('1')},'showingDeer')
-
-    .to('.wrapper-back-position', 0.5, {clip: backWrap[0]},'smashingTo')
-    .to('.wrapper-back-position', 0.5, {clip: backWrap[1], opacity: ('1')},'showingDeer')
-
-    .to('.wrapper-background-position', 0.5, {clip: insideWrap[0]},'smashingTo')
-    .to('.shine-back-position', 0.2, {clip: shinningTo[0]},'smashingTo')
-    .to('.wrapper-background-position', 0.5, {clip: insideWrap[1], opacity: ('1')},'showingDeer')
-
-    .from('.charact-appear-position', 0.8, {ease: Back.easeOut.config(1), top: ('200')},'showingDeer')
+    .addLabel('fallingConfeti')
+    .to('.second-phrase-position', 1, {opacity: ('1')})
+    .staggerTo('.first-confeti-falling', 1, {y: ('300'), rotation: ('100')}, 0.1,'fallingConfeti')
+    .staggerTo('.second-confeti-falling', 1.2, {y: ('300'), rotation: ('-200')}, 0.2,'fallingConfeti')
+    .staggerTo('.third-confeti-falling', 1.3, {y: ('300'), rotation: ('200')}, 0.1,'fallingConfeti')
+    .staggerTo('.fourth-confeti-falling', 1.4, {y: ('300'), rotation: ('-100')}, 0.2,'fallingConfeti')
     
-    .to('.shine-back-position', 0.5, {clip: shinningTo[1], opacity: ('1')}, 'showingDeer')
-    // /////////////////
+    // Catch Phrase.
+    // /////////////
 
-    // Catchphrase appear animation.
-    // ///////////
-    .from('.catchphrase-text-position', 0.5, {opacity: ('0')}, 'smashingTo')
-    .from('.catchphrase-shade-position', 0.5, {opacity: ('0')}, 'smashingTo')
-    // ///////////
+    // Mr.Peanut appears.
+    // //////////////////
+    .to('.door-opened-position', 0.2, {opacity: ('1')}, 'fallingConfeti')
+    .to(['.ballons-position', '.mrPeanut-body-position'], 0.5, {opacity: ('1')}, 'fallingConfeti')
+    // Mr.Peanut appears.
+    // //////////////////
+
+    // Zooming.
+    // ////////
+    .addLabel('zooming', '-=1.5')
+    .to('.mrPeanut-body-position', 0.5, {top: ('21'), left: ('13'), scale: ('1.1')}, 'zooming')
+    .to('.ballons-position', 0.5, {top: ('-102'), left: ('-54'), scale: ('1.1')}, 'zooming')
+    .to(['.street-lines-position', '.nutmovile-position', '.door-opened-position', '.second-phrase-position'], 0.5, {opacity: ('0')}, 'zooming')
+    .to('.third-phrase-position', 1, {opacity: ('1')}, 'zooming')
+    // Zooming.
+    // ////////
 
     .from('.final-productshot-position', 0.5, {left: ('350'), onComplete: actionsButton})
 }
 
 function actionsButton(){
+    TweenMax.to(_btnExit, 1,{zIndex: ('100')})
     _btnExit.addEventListener('mouseover', () => {
-        TweenMax.to(_btnExit, 1,{zIndex: ('100')})
-        TweenMax.to('.charact-appear-position', 1,{top: ('-5')})
-        TweenMax.to('.shine-back-position', 1,{top: ('-20')})
+        TweenMax.to('.call-action-banner', 1, {opacity: ('1')})
+        TweenMax.to('.mrPeanut-eye-position', 0.2, {opacity: ('1')})
     });
     _btnExit.addEventListener('mouseout', () => {
-        TweenMax.to(_btnExit, 1,{zIndex: ('100')})
-        TweenMax.to('.charact-appear-position', 1,{top: ('0')})
-        TweenMax.to('.shine-back-position', 1,{top: ('0')})
+        TweenMax.to('.call-action-banner', 0.5, {opacity: ('0')})
+        TweenMax.to('.mrPeanut-eye-position', 0.2, {opacity: ('0')})
     });
     const looperBanner = function() {
         location.reload();
     }
-    setTimeout(looperBanner, 5000);
+    setTimeout(looperBanner, 10000);
 }
+
+
+function generateRandomValue (maxValue) {
+    let maxNumb = maxValue;
+    let randomNumb = parseInt(Math.random() * maxNumb);
+    return randomNumb;
+}
+
+// Generate confeti.
+// This functionality is working properly.
+// /////////////////
+
+const maxRandNumb = 100;
+
+const colorsConfeti = [
+    'blue',
+    'red',
+    'pink',
+    'green',
+    'deepPink',
+    'lightBlue',
+    'yellow',
+    'orange',
+];
+
+function confetiColorGenerator() {
+    return colorsConfeti[generateRandomValue (colorsConfeti.length)];
+}
+
+function confetiSizeGenerator() {
+    let sizer = generateRandomValue (20);
+    return sizer + 'px';
+}
+
+function confetiLetfPosition() {
+    return generateRandomValue (maxRandNumb)+ '%';
+}
+
+function confetiBorder(maxBorderNumb) {
+    return (generateRandomValue (maxBorderNumb)+ 'px',generateRandomValue (maxBorderNumb)+ 'px',generateRandomValue (maxBorderNumb)+ 'px',generateRandomValue (maxBorderNumb)+ 'px');
+}
+
+function confetiTemplate( bodyColorKey, widthBodyKey, positionBodyKey, topInitKey, leftInitKey, indexKey, opacityKey, classKey, roundBorderKey) {
+    this.bodyColorKey = bodyColorKey;
+    this.widthBodyKey = widthBodyKey;
+    this.positionBodyKey = positionBodyKey;
+    this.topInitKey = topInitKey;
+    this.leftInitKey = leftInitKey;
+    this.indexKey = indexKey;
+    this.opacityKey = opacityKey;
+    this.classKey = classKey;
+    this.roundBorderKey = roundBorderKey;
+
+    const confetiWrapper = document.getElementById('js-confeti-bucket');
+    const confeti = document.createElement('div');
+    confeti.setAttribute('class', this.classKey)
+
+    confeti.style.backgroundColor = this.bodyColorKey;
+
+    confeti.style.width = this.widthBodyKey,
+    confeti.style.height = this.widthBodyKey,
+
+    confeti.style.position = this.positionBodyKey,
+    confeti.style.top = this.topInitKey,
+    confeti.style.left = this.leftInitKey,
+
+    confeti.style.zIndex = this.indexKey,
+    confeti.style.opacity = this.opacityKey;
+
+    confeti.style.borderRadius = this.roundBorderKey;
+
+    confetiWrapper.appendChild(confeti);
+}
+
+function firstConfetiGroup() {
+    for(let e = 0; e < 25; e++) {
+        new confetiTemplate( confetiColorGenerator(), confetiSizeGenerator(), 'absolute', '-50px', confetiLetfPosition(), '5', '0.7', 'first-confeti-falling', confetiBorder(3))
+    }
+}
+function secondConfetiGroup() {
+    for(let e = 0; e < 25; e++) {
+        new confetiTemplate( confetiColorGenerator(), confetiSizeGenerator(), 'absolute', '-50px', confetiLetfPosition(), '5', '0.7', 'second-confeti-falling', confetiBorder(4))
+    }
+}
+function thirdConfetiGroup() {
+    for(let e = 0; e < 25; e++) {
+        new confetiTemplate( confetiColorGenerator(), confetiSizeGenerator(), 'absolute', '-50px', confetiLetfPosition(), '5', '0.7', 'third-confeti-falling', confetiBorder(5))
+    }
+}
+function fourthConfetiGroup() {
+    for(let e = 0; e < 25; e++) {
+        new confetiTemplate( confetiColorGenerator(), confetiSizeGenerator(), 'absolute', '-50px', confetiLetfPosition(), '5', '0.7', 'fourth-confeti-falling', confetiBorder(3))
+    }
+}
+
+secondConfetiGroup();
+firstConfetiGroup();
+thirdConfetiGroup();
+fourthConfetiGroup();
